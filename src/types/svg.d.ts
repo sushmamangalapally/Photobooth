c7984160978610913ce2f1671587aba479f7219b
Sushma Mangalapally <sushma.m812@gmail.com>
2025-10-12 00:05:26 -0700

    Clean up code

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,7 +1,7 @@
-
-    declare module '*.svg' {
-      import React = require('react');
-      export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-      const src: string;
-      export default src;
-    }
+/* eslint-disable @typescript-eslint/no-require-imports */
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+  }
